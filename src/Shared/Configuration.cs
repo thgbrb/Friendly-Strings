b9b1f1cf88ec20a238363c29c9df22adcf566379
Thiago Borba <thiago.borba@cwi.com.br>
2020-02-20 00:04:21 -0300

    pptx updated

--- src/Shared/Configuration.cs
+++ src/Shared/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class Configuration
     {
-        public const string SOURCE_FILE = "D:\\working\\dados\\gastos2018.csv";
+        public const string SOURCE_FILE = "D:\\working\\dados\\gastos122018.csv";
         public const string DATABASE_FILE = "D:\\working\\dados\\database.csv";
         public const int NUMBER_OF_COLUMS = 63; // Numbers of colums into csv file
         public const char SEPARATOR = ';';
